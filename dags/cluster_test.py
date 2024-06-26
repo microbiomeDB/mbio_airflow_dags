20363dcb52f3cf071232da4c76b69133493f9cbc
Danielle Callan <dcallan@upenn.edu>
2024-06-26 13:24:11 -0400

    make clusterLogin a user param

--- dags/cluster_test.py
+++ dags/cluster_test.py
@@ -1,7 +1,6 @@
 import json
 import pendulum
 
-from include.src.date.decorator import DefaultDateTime
 from airflow import DAG
 from include.src.airflow.xcom import cleanup
 from mbio_airflow_dags.utils.ClusterManager import ClusterManager
@@ -12,16 +11,17 @@ with DAG(
         start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
         catchup=False,
         on_failure_callback=cleanup,
-        on_success_callback=cleanup
+        on_success_callback=cleanup,
+        params={'clusterLogin': 'dcallan'}
 ) as dag:
     # TODO make this configurable by user somehow? and not hard coded..
     # ideally a user would just use the clusterName 'pmacs' and wed find the right config
-    cluster_config = json.load(open('/data/MicrobiomeDB/mbio-airflow-dags/cluster_configs/pmacs.json'))   
+    cluster_config = json.load(open('/data/MicrobiomeDB/mbio-airflow-dags/cluster_configs/pmacs.json'))
     cluster_manager = ClusterManager(
         cluster_config['headNode'], 
         cluster_config['fileTransferNode'], 
         cluster_config['clusterType'], 
-        cluster_config['clusterLogin']
+        "{{params.clusterLogin}}"
     )
 
     # make a dummy file to test with
